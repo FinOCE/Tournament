186f654367e508e7cae4d60dc13333fc4a6d0cbf
Finley Sherwood <me@itsf.in>
2022-06-05 13:46:01 +1000

    Add tests for bracket structures

--- API.Tests.Unit/Models/Brackets/Structures/FinaleTest.cs
+++ API.Tests.Unit/Models/Brackets/Structures/FinaleTest.cs
@@ -3,21 +3,81 @@
 [TestClass]
 public class FinaleTest
 {
+    SnowflakeService SnowflakeService = null!;
+    Finale Structure = null!;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        SnowflakeService = new();
+        Series series = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure = new(series, new(1), new(2));
+    }
+
     [TestMethod]
-    [Ignore]
     public void ConstructorTest()
     {
+        // Assert
+        Assert.IsInstanceOfType(
+            Structure.Series.WinnerProgression,
+            typeof(ExactPlacement),
+            "The winner progression should be an exact placement");
+
+        Assert.AreEqual(
+            1,
+            ((ExactPlacement)Structure.Series.WinnerProgression!).Position,
+            "The winner progression placement should be 1");
+
+        Assert.IsInstanceOfType(
+            Structure.Series.LoserProgression,
+            typeof(ExactPlacement),
+            "The loser progression should be an exact placement");
+
+        Assert.AreEqual(
+            2,
+            ((ExactPlacement)Structure.Series.LoserProgression!).Position,
+            "The loser progression placement should be 2");
     }
 
     [TestMethod]
-    [Ignore]
     public void SetWinnerProgressionTest()
     {
+        // Arrange
+        ExactPlacement placement = new(4);
+
+        // Act
+        Structure.SetWinnerProgression(placement);
+
+        // Assert
+        Assert.AreNotEqual(
+            4,
+            ((ExactPlacement)Structure.Series.WinnerProgression!).Position,
+            "The winner progression should not have updated to 4");
+
+        Assert.AreEqual(
+            1,
+            ((ExactPlacement)Structure.Series.WinnerProgression!).Position,
+            "The winner progression should still be 1");
     }
 
     [TestMethod]
-    [Ignore]
     public void SetLoserProgressionTest()
     {
+        // Arrange
+        ExactPlacement placement = new(4);
+
+        // Act
+        Structure.SetLoserProgression(placement);
+
+        // Assert
+        Assert.AreNotEqual(
+            4,
+            ((ExactPlacement)Structure.Series.LoserProgression!).Position,
+            "The loser progression should not have updated to 4");
+
+        Assert.AreEqual(
+            2,
+            ((ExactPlacement)Structure.Series.LoserProgression!).Position,
+            "The loser progression should still be 2");
     }
 }
--- API.Tests.Unit/Models/Brackets/Structures/StructureTest.cs
+++ API.Tests.Unit/Models/Brackets/Structures/StructureTest.cs
@@ -3,39 +3,163 @@
 [TestClass]
 public class StructureTest
 {
+    SnowflakeService SnowflakeService = null!;
+    Structure Structure = null!;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        SnowflakeService = new();
+        Series series = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure = new(series);
+    }
+
     [TestMethod]
-    [Ignore]
     public void ChildrenTest()
     {
+        // Arrange
+        Series series1 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure1 = new(series1);
+        Series series2 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure2 = new(series2);
+        Series series3 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure3 = new(series3);
+
+        // Act
+        Structure.AddChild(structure1);
+        Structure.AddChild(structure2);
+        structure1.AddChild(structure3);
+
+        // Asset
+        Assert.AreEqual(3, Structure.Children, "There should be 3 structures as children");
     }
 
     [TestMethod]
-    [Ignore]
     public void SetWinnerProgressionTest()
     {
+        // Arrange
+        ExactPlacement placement = new(1);
+
+        // Act
+        Structure.SetWinnerProgression(placement);
+
+        // Assert
+        Assert.IsNotNull(Structure.Series.WinnerProgression, "The series winner progression should be set");
     }
 
     [TestMethod]
-    [Ignore]
     public void SetLoserProgressionTest()
     {
+        // Arrange
+        ExactPlacement placement = new(2);
+
+        // Act
+        Structure.SetLoserProgression(placement);
+
+        // Assert
+        Assert.IsNotNull(Structure.Series.LoserProgression, "The series winner progression should be set");
     }
 
     [TestMethod]
-    [Ignore]
     public void FindStructureWithTeamTest_Single()
     {
+        // Arrange
+        Team team = new(SnowflakeService.Generate().ToString(), "Team 1", null, false);
+        Structure.Series.AddTeam(team);
+
+        // Act
+        IStructure? structure = Structure.FindStructureWithTeam(team.Id);
+
+        // Assert
+        Assert.IsNotNull(structure, "The structure itself should be found");
+        Assert.IsTrue(structure.Series.Teams.ContainsKey(team.Id), "Team 1 should be in the series");
     }
 
     [TestMethod]
-    [Ignore]
-    public void FindStructureWithTeamTest_Nested()
+    public void FindStructureWithTeamTest_StartNested()
     {
+        // Arrange
+        Series series1 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure1 = new(series1);
+        Series series2 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure2 = new(series2);
+        Series series3 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure3 = new(series3);
+
+        Team team = new(SnowflakeService.Generate().ToString(), "Team 1", null, false);
+        structure3.Series.AddTeam(team);
+
+        Structure.AddChild(structure1);
+        Structure.AddChild(structure2);
+        structure1.AddChild(structure3);
+
+        // Act
+        IStructure? structure = Structure.FindStructureWithTeam(team.Id);
+
+        // Assert
+        Assert.IsNotNull(structure, "The structure itself should be found");
+        Assert.IsTrue(structure.Series.Teams.ContainsKey(team.Id), "Team 1 should be in the nested series");
     }
 
     [TestMethod]
-    [Ignore]
+    public void FindStructureWithTeamTest_ProgressNested()
+    {
+        // Arrange
+        Series series1 = new(SnowflakeService.Generate().ToString(), null, 1);
+        Structure structure1 = new(series1);
+        Series series2 = new(SnowflakeService.Generate().ToString(), null, 1);
+        Structure structure2 = new(series2);
+        Series series3 = new(SnowflakeService.Generate().ToString(), null, 1);
+        Structure structure3 = new(series3);
+
+        Structure.AddChild(structure1);
+        structure1.SetWinnerProgression(Structure.Series);
+        Structure.AddChild(structure2);
+        structure2.SetWinnerProgression(Structure.Series);
+        structure1.AddChild(structure3);
+        structure3.SetWinnerProgression(structure1.Series);
+
+        Team team1 = new(SnowflakeService.Generate().ToString(), "Team 1", null, false);
+        Team team2 = new(SnowflakeService.Generate().ToString(), "Team 2", null, false);
+        structure3.Series.AddTeam(team1);
+        structure3.Series.AddTeam(team2);
+
+        structure3.Series.Start();
+        Game game = new(SnowflakeService.Generate().ToString(), structure3.Series);
+        game.SetScore(team1.Id, 1);
+        game.Finish();
+        structure3.Series.Finish();
+
+        // Act
+        IStructure? structure = Structure.FindStructureWithTeam(team1.Id);
+
+        // Assert
+        Assert.IsNotNull(structure, "The structure itself should be found");
+        Assert.IsTrue(structure.Series.Teams.ContainsKey(team1.Id), "Team 1 should be in the nested series");
+        Assert.IsTrue(Structure.Right!.Series.Teams.ContainsKey(team1.Id), "Team 1 should have progressed to semi-finals");
+    }
+
+    [TestMethod]
     public void AddChildTest()
     {
+        // Arrange
+        Series series1 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure1 = new(series1);
+        Series series2 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure2 = new(series2);
+        Series series3 = new(SnowflakeService.Generate().ToString(), null, 3);
+        Structure structure3 = new(series3);
+
+        // Act
+        bool firstSuccessful = Structure.AddChild(structure1);
+        bool secondSuccessful = Structure.AddChild(structure2);
+        bool thirdSuccessful = Structure.AddChild(structure3);
+
+        // Assert
+        Assert.IsTrue(firstSuccessful, "First child should be added successfully");
+        Assert.AreEqual(structure1.Series.Id, Structure.Right!.Series.Id, "First child should be on the right");
+        Assert.IsTrue(secondSuccessful, "Second child should be added successfully");
+        Assert.AreEqual(structure2.Series.Id, Structure.Left!.Series.Id, "Second child should be on the left");
+        Assert.IsFalse(thirdSuccessful, "Third child should not be added successfully");
     }
 }
